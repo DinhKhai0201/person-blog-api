a4015f54327bbabe8562ca278a6ff8ca2574ead7
Kai1997 <nguyendinhkhai2111997@gmail.com>
2020-05-07 13:47:58 +0700

    test1

--- configs/mongoose.config.js
+++ configs/mongoose.config.js
@@ -12,8 +12,9 @@ module.exports.loadModels = () => {
 module.exports.connect = () => {
     mongoose.Promise = global.Promise;
     return new Promise((resolve, reject) => {
-        let db = mongoose.connect(dbcConfig.uri, dbcConfig.options).then(() => {
+        let db = mongoose.connect(process.env.MONGODB_URL, dbcConfig.options).then(() => {
             console.log("connected")
+            console.log("db", db)
             resolve(db);
         }).catch((err) => {
             console.log(err)
